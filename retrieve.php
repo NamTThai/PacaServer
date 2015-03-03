e9ca97594df8d65d525122c44064122c495ac039
NamTThai <nam.t.thai@espn.com>
2015-03-02 19:10:09 -0500

    Server working

--- retrieve.php
+++ retrieve.php
@@ -1,7 +1,7 @@
 <?php
 
 // This file is a simple HTTP GET to retrieve all links to pictures in the Pictures directory
-$test_url = "Server response";
+$test_url = "Your beloved server says hi :D";
 
 echo $test_url;
 
