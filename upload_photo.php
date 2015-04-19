55ec3627e84d15a8ca5cbd4a6326c05b8ddeabbe
NamTThai <nam.t.thai@espn.com>
2015-04-19 14:55:11 -0400

    Done with upload and unserialize file

--- upload_photo.php
+++ upload_photo.php
@@ -1,12 +1,23 @@
 <?php
-
+require_once 'class.Picture.php';
+    
     $file_path = "../Pictures/";
 
     $file_path = $file_path . basename( $_FILES['uploaded_file']['name']);
-    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $file_path)) {
+
+    $head = unserialize(file_get_contents("../Pictures/data.Head.php"));
+
+    $newHead = new Picture($file_path);
+    $newHead->setNext($head);
+
+    $objData = serialize($newHead);
+    $file = fopen("../Pictures/data.Head.php", "w");
+    fwrite($file, $objData);
+    fclose($file);
+
+    if (move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $file_path)) {
         echo "success";
     } else{
         echo "fail";
     }
-
 ?>
