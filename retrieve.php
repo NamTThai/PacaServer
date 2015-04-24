a1553dc6de187b0c7212800ac7f36751b9d1b070
NamTThai <nam.thai.cs@gmail.com>
2015-04-24 08:41:49 -0400

    Server response with array of JSONObject

--- retrieve.php
+++ retrieve.php
@@ -7,13 +7,7 @@ require_once 'utils/class.PacaDb.php';
     
     $addresses = $db->retrievePictures($_GET['lat'], $_GET['lng']);
 
-    $allPictures = array();
-    
-    for ($i = 0; $i < count($addresses); $i++) {
-        array_push($allPictures, $addresses[$i]['address']);
-    }
-
-    echo json_encode($allPictures);
+    echo json_encode($addresses);
     
     $db->close();
 
